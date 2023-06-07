3fc0813228bc3531cc9f0971d7698f41aeae1635
Vivian Lu <vivianlu512@gmail.com>
2023-06-07 12:40:33 +0800

    add firebase

---
+++ spotify/src/firebase/index.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+}; // import.meta.env = process.env,在vite將process改成meat
+
+const app = initializeApp(firebaseConfig); //初始化
+const db = getFirestore(firebaseApp); //取得資料庫
+const analytics = getAnalytics(app); //取得分析
+const auth = getAuth(); //取得身分驗證機制
+
+export { auth, db, analytics };
