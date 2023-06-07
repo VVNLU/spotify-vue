2f27c28abc2f1ea9db7f08b36c71657e3444464f
Vivian Lu <vivianlu512@gmail.com>
2023-06-07 18:42:21 +0800

    add mdi icon

--- spotify/src/main.js
+++ spotify/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
+import "@mdi/font/css/materialdesignicons.css";
 
 const app = createApp(App);
 
