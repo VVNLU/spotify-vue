f3ebbcb2abe0875d8718c07f4842e6da1fe5180f
Vivian Lu <vivianlu512@gmail.com>
2023-06-07 12:53:05 +0800

    add router

---
+++ spotify/src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../pages/HomePage.vue";
+import LoginPage from "../pages/LoginPage.vue";
+import RegisterPage from "../pages/RegisterPage.vue";
+import ForgetPasswordPage from "../pages/ForgetPasswordPage.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomePage,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginPage,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: RegisterPage,
+    },
+    {
+      path: "/forgetpassword",
+      name: "forgetpassword",
+      component: ForgetPasswordPage,
+    },
+  ],
+});
+export default router;
