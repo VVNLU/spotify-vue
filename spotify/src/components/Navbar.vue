b48864f27dd919f09ad4537415f2102ee98ec140
Vivian Lu <vivianlu512@gmail.com>
2023-06-09 22:56:11 +0800

    add navbar

---
+++ spotify/src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="navbar">
+    <div class="logo"><img src="../assets/logo/logo.png" alt="" /></div>
+    <div class="connect">
+      <router-link to="">Premium</router-link>
+      <router-link to="">支援</router-link>
+      <router-link to="">下載</router-link>
+      <span>｜</span>
+      <router-link to="register">註冊</router-link>
+      <router-link to="login">登入</router-link>
+    </div>
+  </div>
+</template>
+<style scoped>
+.navbar {
+  display: grid;
+  height: 80px;
+  align-content: center;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: bold;
+  grid-template-columns: repeat (7, 1fr);
+  background-color: #000;
+}
+.logo {
+  grid-column: 2/3;
+}
+img {
+  width: 150px;
+}
+.connect {
+  grid-column: 8/8;
+  color: white;
+}
+a,
+.router-link-active {
+  display: inline-flex;
+  text-decoration: none;
+  color: white;
+  margin: 15px;
+  align-items: center;
+}
+a:hover,
+.router-link-active:hover {
+  color: #1ed760;
+}
+/* @media screen and (max-width: 700px) {
+  a,
+  .router-link-active {
+    font-family: "Material Icons";
+    content: "mdi-menu";
+  }
+} */
+</style>
