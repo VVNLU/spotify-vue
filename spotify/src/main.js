5e0d0fe45154353e9468b630d37685da32b5462f
Vivian Lu <vivianlu512@gmail.com>
2023-06-17 11:46:23 +0800

    add faker js

--- spotify/src/main.js
+++ spotify/src/main.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
 import "@mdi/font/css/materialdesignicons.css";
+import { faker } from "@faker-js/faker";
 
 const app = createApp(App);
 
+app.use(faker);
 app.use(router);
 app.mount("#app");
