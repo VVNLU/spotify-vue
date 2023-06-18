d9126a116e636ae5ddeba7badc42b971a7d1a08b
Vivian Lu <vivianlu512@gmail.com>
2023-06-18 16:00:41 +0800

    add fake info

--- spotify/src/components/HomeContent.vue
+++ spotify/src/components/HomeContent.vue
@@ -2,17 +2,37 @@
   <div class="content">
     <div class="focus" v-for="n in 3">
       <div class="title">專注</div>
-      <div class="musicBox" v-for="n in 4">
+      <div class="musicBox" v-for="item in data">
         <a class="action-label icon"><i class="mdi mdi-play-circle"></i></a>
-        <img src="../assets/album/peacefulPiano.png" alt="peaceful piano" />
-        <span class="musicTitle">Peaceful Piano</span>
-        <span class="musicContent">
-          Peaceful piano to help you slow down, breathe, and relax.
-        </span>
+        <img :src="item.image" />
+        <span class="musicTitle">{{ item.fullName }}</span>
+        <span class="musicContent">Artist．{{ item.genre }}</span>
       </div>
     </div>
   </div>
 </template>
+<script setup>
+import { faker } from "@faker-js/faker";
+const generateFakerMusic = () => {
+  const box = [];
+  for (let i = 1; i <= 4; i++) {
+    const image = faker.image.url({
+      height: 200,
+      width: 200,
+    });
+    const fullName = faker.person.fullName();
+    const genre = faker.music.genre();
+    box.push({
+      image: image,
+      fullName: fullName,
+      genre: genre,
+    });
+  }
+  return box;
+};
+console.log(generateFakerMusic());
+const data = generateFakerMusic();
+</script>
 <style scoped>
 .content {
   width: 100%;
@@ -59,15 +79,12 @@ i {
 }
 .musicTitle {
   font-weight: bold;
-  /* text-indent: 20px; */
 }
 .musicContent {
+  display: flex;
+  justify-content: center;
   margin-top: 10px;
   width: 170px;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
   color: grey;
   font-size: 14px;
 }
