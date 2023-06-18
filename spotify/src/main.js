feefaf43238000dd29018ce9b151049b6653d29e
Vivian Lu <vivianlu512@gmail.com>
2023-06-18 15:59:45 +0800

    delete faker js

--- spotify/src/main.js
+++ spotify/src/main.js
@@ -2,10 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
 import "@mdi/font/css/materialdesignicons.css";
-import { faker } from "@faker-js/faker";
 
 const app = createApp(App);
 
-app.use(faker);
 app.use(router);
 app.mount("#app");
