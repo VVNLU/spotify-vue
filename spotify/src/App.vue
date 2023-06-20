2b36c6c3d3db15776bc4f9d1aba312df10eb4531
Vivian Lu <vivianlu512@gmail.com>
2023-06-20 12:43:11 +0800

    add the layout and slot

--- spotify/src/App.vue
+++ spotify/src/App.vue
@@ -1,3 +1,42 @@
 <template>
-  <RouterView />
+  <template v-if="$route.name === 'home'">
+    <header class="header">
+      <HomeNavbar />
+    </header>
+    <aside class="rightBar">
+      <Rightbar />
+    </aside>
+    <TheLayout>
+      <RouterView />
+    </TheLayout>
+  </template>
+  <template v-else>
+    <router-view></router-view>
+  </template>
 </template>
+<script setup>
+import TheLayout from "./components/TheLayout.vue";
+import HomeNavbar from "./components/HomeNavbar.vue";
+import Rightbar from "./components/Rightbar.vue";
+</script>
+<style scoped>
+* {
+  position: relative;
+}
+.header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 80%;
+  z-index: 1;
+}
+.rightBar {
+  position: fixed;
+  margin: 0;
+  top: 0;
+  left: 0;
+  width: 20%;
+  height: 100vh;
+  z-index: 2;
+}
+</style>
---
+++ spotify/src/components/TheLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="content">
+    <slot></slot>
+  </div>
+</template>
+<style scoped>
+.content {
+  width: 100%;
+  margin-top: 80px;
+  margin-left: 20%;
+}
+</style>
