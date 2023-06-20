e12dd8b8f296f63063633d8163a5abe61f207983
Vivian Lu <vivianlu512@gmail.com>
2023-06-20 12:43:59 +0800

    delete the homebar and right bar

--- spotify/src/pages/HomePage.vue
+++ spotify/src/pages/HomePage.vue
@@ -1,19 +1,11 @@
 <template>
   <div class="home">
-    <HomeNavbar style="grid-column: 2/4; grid-row: 1/1" />
-    <Rightbar style="grid-column: 1/1; grid-row: 1/5" />
-    <HomeContent style="grid-column: 2/4; grid-row: 2/5" />
+    <HomeContent />
   </div>
+  <!-- <HomeAlbum /> -->
 </template>
 <script setup>
-import HomeNavbar from "../components/HomeNavbar.vue";
-import Rightbar from "../components/Rightbar.vue";
 import HomeContent from "../components/HomeContent.vue";
+import HomeAlbum from "../components/HomeAlbum.vue";
 </script>
-<style scoped>
-* {
-  display: grid;
-  grid-template-columns: repeat (auto, 100px);
-  grid-template-rows: repeat (auto, 100px);
-}
-</style>
+<style scoped></style>
