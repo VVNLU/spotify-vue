58b14cb818d5cbee9979bdf9ef26923dd89431a8
Vivian Lu <vivianlu512@gmail.com>
2023-06-21 21:43:21 +0800

    add vue3-Cookies

--- spotify/src/main.js
+++ spotify/src/main.js
@@ -2,8 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
 import "@mdi/font/css/materialdesignicons.css";
+import VueCookies from "vue3-cookies";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(VueCookies, {
+  expireTimes: "30d",
+  //   path: "/",
+  domain: "",
+  secure: true,
+  sameSite: "None",
+});
 app.mount("#app");
