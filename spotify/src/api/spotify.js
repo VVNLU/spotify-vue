0817d91ce1e41edffefc87161020f7eeb5d05033
Vivian Lu <vivianlu512@gmail.com>
2023-06-27 22:39:56 +0800

    export getPlaylistItems

--- spotify/src/api/spotify.js
+++ spotify/src/api/spotify.js
@@ -10,3 +10,8 @@ export const getCategoryPlaylists = async () => {
   const response = await api.get("browse/categories/toplists/playlists");
   return response.data;
 };
+
+export const getPlaylistItems = async () => {
+  const response = await api.get("playlists/37i9dQZF1DWWqC43bGTcPc/tracks");
+  return response.data;
+};
