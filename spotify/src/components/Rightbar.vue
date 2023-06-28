36d8d5be05d3217c88b3e3e02433151ccccf318f
Vivian Lu <vivianlu512@gmail.com>
2023-06-28 19:28:37 +0800

    update typography and CSS

--- spotify/src/components/Rightbar.vue
+++ spotify/src/components/Rightbar.vue
@@ -1,87 +1,148 @@
 <template>
-  <div class="rightbar">
-    <div class="logo"><img src="../assets/logo/logo.png" alt="" /></div>
-    <div class="navbarList">
-      <router-link to="/">
-        <a class="action-label icon">
-          <i class="mdi mdi-home-variant-outline"></i></a
-        ><span>首頁</span></router-link
-      >
-      <router-link to="">
-        <a class="action-label icon"> <i class="mdi mdi-magnify"></i></a
-        ><span>搜尋</span>
-      </router-link>
-      <router-link to="">
+  <div class="sidebar">
+    <div class="topbar">
+      <div class="routerBtn homeRouter">
+        <router-link to="/">
+          <a class="action-label icon">
+            <i class="mdi mdi-home-variant-outline"></i
+          ></a>
+          <span class="searchTitle">首頁</span></router-link
+        >
+      </div>
+      <div class="routerBtn searchRouter">
+        <router-link to="search">
+          <a class="action-label icon"><i class="mdi mdi-magnify"></i></a>
+          <span class="searchTitle">搜尋</span>
+        </router-link>
+      </div>
+    </div>
+    <div class="bottombar">
+      <div class="favorMusic">
         <a class="action-label icon">
           <i class="mdi mdi-music-box-multiple-outline"></i></a
-        ><span>你的音樂庫</span></router-link
-      >
-      <br />
-      <router-link to="">
-        <a class="action-label icon">
-          <i class="mdi mdi-plus-box-multiple-outline"></i></a
-        ><span>建立播放清單</span>
-      </router-link>
-      <router-link to="">
-        <a class="action-label icon"> <i class="mdi mdi-heart-outline"></i></a
-        ><span>已按讚的歌曲</span>
-      </router-link>
-    </div>
-    <br />
-    <div class="legalList">
-      <router-link to="">法律條款</router-link>
-      <router-link to="">隱私權中心</router-link><br /><br />
-      <router-link to="">隱私政策</router-link>
-      <router-link to="">Cookie</router-link>
-      <router-link to="">關於廣告</router-link><br /><br />
-      <router-link to="">無障礙</router-link><br /><br />
-      <router-link to="">Cookie</router-link>
+        ><span>你的音樂庫</span
+        ><a class="action-label icon"> <i class="mdi mdi-plus"></i></a>
+      </div>
+      <div class="musicboxLists">
+        <div class="createlists">
+          <p class="createTitle">建立你的第一個播放清單</p>
+          <p class="createContent">這很簡單,我們會幫你</p>
+          <button class="createBtn">建立播放清單</button>
+        </div>
+        <div class="createlists">
+          <p class="createTitle">不妨專注一些Podcast</p>
+          <p class="createContent">我們會在新單即推出時通知你</p>
+          <button class="createBtn">瀏覽Podcast</button>
+        </div>
+      </div>
+      <div class="legalList">
+        <a href="https://www.spotify.com/tw/legal/end-user-agreement/"
+          >法律條款</a
+        >
+        <a href="https://www.spotify.com/tw/privacy">隱私權中心</a>
+        <a href="https://www.spotify.com/tw/legal/privacy-policy/">隱私政策</a
+        ><br />
+        <a href="https://www.spotify.com/tw/legal/privacy-policy/">Cookie</a>
+        <a href="https://www.spotify.com/tw/legal/privacy-policy/">關於廣告</a>
+        <a href="https://www.spotify.com/tw/accessibility">無障礙</a><br />
+        <a href="https://www.spotify.com/tw/accessibility" class="cookie"
+          >Cookie</a
+        >
+      </div>
     </div>
   </div>
 </template>
-<style sceoped>
-.rightbar {
-  display: grid;
-  grid-template-rows: repeat (25, 50px);
-  background-color: #000;
-  width: 100%;
-  height: 100%;
+<style scoped>
+.sidebar {
+  height: 85%;
+  position: relative;
+}
+.topbar {
+  border-radius: 10px;
+  background-color: #121212;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-}
-.logo {
-  grid-row: 1/1;
-}
-img {
-  width: 130px;
-  margin: 20px;
-}
-.navbarList {
-  grid-row: 2/2;
-  display: flex;
-  flex-direction: column;
   font-weight: bold;
-  font-size: 14px;
+  margin: 10px;
+  padding: 15px;
 }
-.navbarList > a {
+.homeRouter,
+.routerBtn > a {
+  text-decoration: none;
+  color: #b3b3b3;
   display: flex;
   align-items: center;
-  text-decoration: none;
-  color: grey;
-  margin: 5px 20px;
 }
-.navbarList span {
-  margin-left: 15px;
+.searchTitle {
+  margin: 0 0 0 15px;
 }
-.navbarList a:hover {
+.bottombar {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  background-color: #121212;
+  border-radius: 10px;
+  margin: 10px;
+  height: 100%;
+}
+.favorMusic {
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  color: #b3b3b3;
+  font-weight: bold;
+}
+@media (min-height: 500px) {
+  .musicboxLists {
+    height: 150px;
+    overflow: auto;
+    white-space: nowrap;
+  }
+}
+::-webkit-scrollbar {
+  width: 10px;
+  background-color: transparent;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background-color: #5a5a5a;
+}
+
+.createlists {
+  margin: 10px;
+  border-radius: 10px;
   color: white;
+  background-color: #242424;
+  padding: 5px 0 20px 20px;
+}
+.createTitle {
+  font-weight: bold;
+}
+.createContent {
+  font-size: 14px;
+}
+.createBtn {
+  height: 35px;
+  width: 120px;
+  border-radius: 50px;
+  border: none;
+  font-weight: bold;
+}
+.createBtn:hover {
+  transform: scale(1.05);
+  cursor: pointer;
 }
 .legalList {
-  grid-row: 20/25;
+  position: absolute;
+  margin: 10px;
+  line-height: 2em;
+  bottom: 30px;
 }
 .legalList > a {
   font-size: 12px;
   text-decoration: none;
   color: grey;
   margin-left: 20px;
+}
+.cookie:hover {
+  text-decoration: underline 1px;
 }
 </style>
