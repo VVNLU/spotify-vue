9acb6ccb8298bdfe61eceb015ba75b72657f78ee
Vivian Lu <vivianlu512@gmail.com>
2023-06-28 19:30:16 +0800

    motify url

--- spotify/src/api/index.js
+++ spotify/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const token = "0JQ5DAqbMKFQ00XGBls6ym";
+const token = "";
 
 const request = axios.create({
-  baseURL: "https://api.spotify.com/v1",
+  baseURL: "https://api.spotify.com/v1/",
   headers: {
     Authorization: `Bearer ${token}`,
   },
