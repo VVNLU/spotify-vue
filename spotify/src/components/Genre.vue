de80d62e1d323df6f9d47d73214fe0628342fec3
Vivian Lu <vivianlu512@gmail.com>
2023-06-28 20:01:08 +0800

    add the router to album and motify CSS

--- spotify/src/components/Genre.vue
+++ spotify/src/components/Genre.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="genre">
+    <div class="generBigTitle">華語流行</div>
     <div class="genreBox" v-for="item in categoryPlaylists">
-      <a class="action-label icon"><i class="mdi mdi-play-circle"></i></a>
-      <img :src="item.images[0].url" />
-      <span class="genreTitle">{{ item.name }}</span>
-      <span class="genreContent">{{ item.description }}</span>
+      <router-link to="album">
+        <a class="action-label icon"><i class="mdi mdi-play-circle"></i></a>
+        <img :src="item.images[0].url" class="albumImg" />
+        <span class="genreTitle">{{ item.name }}</span>
+        <span class="genreContent">{{ item.description }}</span></router-link
+      >
     </div>
   </div>
 </template>
@@ -21,27 +24,48 @@ onMounted(async () => {
 </script>
 <style scoped>
 .genre {
+  padding: 0 20px;
+  height: 89.3%;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   background-color: #121212;
   color: white;
+  overflow: auto;
+  white-space: wrap;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
 }
-.genreBox > img {
-  border-radius: 5px;
-  width: 80%;
+::-webkit-scrollbar {
+  width: 10px;
+  background-color: black;
+}
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background-color: #5a5a5a;
+}
+.generBigTitle {
+  font-size: 80px;
+  font-weight: bold;
+  margin: 40px 0;
 }
 .genreBox {
   position: relative;
   display: inline-flex;
   flex-direction: column;
-  margin: 20px;
+  margin: 20px 10px;
   width: 200px;
   height: 270px;
   background-color: #181818;
   border-radius: 10px;
+  line-height: 25px;
 }
 .genreBox:hover {
   background-color: #222222;
   cursor: pointer;
+}
+.albumImg {
+  margin: 20px 0 0 20px;
+  border-radius: 5px;
+  width: 80%;
 }
 .genreBox i {
   position: absolute;
@@ -54,7 +78,9 @@ onMounted(async () => {
 .genreBox:hover i {
   display: block;
 }
-
+.genreBox > a {
+  text-decoration: none;
+}
 .genreTitle {
   padding: 0 20px;
   color: white;
