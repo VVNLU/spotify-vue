fb4358c0772666c10df08ced1e17ad402fdd8c1a
Vivian Lu <vivianlu512@gmail.com>
2023-07-02 12:43:53 +0800

    motify router-view

--- spotify/src/pages/HomePage.vue
+++ spotify/src/pages/HomePage.vue
@@ -1,9 +1,8 @@
 <template>
   <HomeContent v-if="$route.name === 'home'" />
-  <HomeAlbum v-else />
+  <RouterView v-else />
 </template>
 <script setup>
 import HomeContent from "../components/HomeContent.vue";
-import HomeAlbum from "../components/HomeAlbum.vue";
 </script>
 <style scoped></style>
