0b12c2f9bcfa551f814214d36b9ecd7a494b33c0
Vivian Lu <vivianlu512@gmail.com>
2023-07-03 21:50:33 +0800

    change the postion of api

--- spotify/src/components/Rightbar.vue
+++ spotify/src/components/Rightbar.vue
@@ -27,8 +27,9 @@
         <div class="createlists">
           <p class="createTitle">建立你的第一個播放清單</p>
           <p class="createContent">這很簡單,我們會幫你</p>
-          <button class="createBtn">建立播放清單</button>
-          <button class="createBtn" @click="getToken()">取得Token</button>
+          <button class="createBtn" @click="getSpotifyToken()">
+            建立播放清單
+          </button>
         </div>
         <div class="createlists">
           <p class="createTitle">不妨專注一些Podcast</p>
@@ -50,56 +51,115 @@
           >Cookie</a
         >
       </div>
+      <div class="translateBtn">
+        <button>
+          <a class="action-label icon"> <i class="mdi mdi-web"></i></a>中文
+        </button>
+      </div>
     </div>
   </div>
 </template>
-<script setup>
+<script>
 import { getAccessToken } from "../api/auth";
 
-const getToken = async () => {
-  console.log(getAccessToken());
+const getSpotifyToken = async () => {
   await getAccessToken();
 };
 </script>
+
 <style scoped>
 .sidebar {
-  height: 85%;
-  position: relative;
+  height: 98%;
+  margin: 2.5%;
+  font-weight: bold;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: white;
+}
+a {
+  text-decoration: none;
+  color: grey;
 }
 .topbar {
-  border-radius: 10px;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   background-color: #121212;
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-  font-weight: bold;
-  margin: 10px;
-  padding: 15px;
+  border-radius: 10px;
+  height: 15%;
+  width: 22%;
 }
-.homeRouter,
 .routerBtn > a {
-  text-decoration: none;
-  color: #b3b3b3;
+  margin-left: 20px;
   display: flex;
   align-items: center;
 }
 .searchTitle {
-  margin: 0 0 0 15px;
+  margin-left: 15px;
 }
 .bottombar {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  position: fixed;
+  display: grid;
+  grid-template-rows: repeat (5 1fr);
   background-color: #121212;
   border-radius: 10px;
-  margin: 10px;
-  height: 100%;
+  top: 18%;
+  width: 22%;
+  height: 81.5%;
 }
 .favorMusic {
-  height: 60px;
+  color: grey;
   display: flex;
   align-items: center;
   justify-content: space-around;
-  color: #b3b3b3;
+}
+.createlists {
+  background-color: #242424;
+  border-radius: 10px;
+  padding: 10px 0 20px 20px;
+  margin: 0 10px 25px 10px;
+}
+.createContent {
+  font-size: 14px;
+}
+.createBtn {
+  height: 35px;
+  width: 120px;
+  border-radius: 50px;
+  border: none;
   font-weight: bold;
 }
-@media (min-height: 500px) {
+.legalList {
+  grid-row: 3/4;
+}
+.legalList > a {
+  font-weight: normal;
+  font-size: 12px;
+  padding: 10px;
+  line-height: 30px;
+}
+.cookie:hover {
+  text-decoration: underline;
+}
+.translateBtn {
+  grid-row: 5/5;
+  padding-left: 10px;
+}
+.translateBtn > button {
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  color: white;
+  border-radius: 50px;
+  border: white 1px solid;
+  background-color: transparent;
+  padding: 5px 15px;
+}
+.translateBtn i {
+  color: white;
+  font-size: 25px;
+}
+@media (max-width: 1280px) {
   .musicboxLists {
     height: 150px;
     overflow: auto;
@@ -113,45 +173,5 @@ const getToken = async () => {
 ::-webkit-scrollbar-thumb {
   border-radius: 10px;
   background-color: #5a5a5a;
-}
-
-.createlists {
-  margin: 10px;
-  border-radius: 10px;
-  color: white;
-  background-color: #242424;
-  padding: 5px 0 20px 20px;
-}
-.createTitle {
-  font-weight: bold;
-}
-.createContent {
-  font-size: 14px;
-}
-.createBtn {
-  height: 35px;
-  width: 120px;
-  border-radius: 50px;
-  border: none;
-  font-weight: bold;
-}
-.createBtn:hover {
-  transform: scale(1.05);
-  cursor: pointer;
-}
-.legalList {
-  position: absolute;
-  margin: 10px;
-  line-height: 2em;
-  bottom: 30px;
-}
-.legalList > a {
-  font-size: 12px;
-  text-decoration: none;
-  color: grey;
-  margin-left: 20px;
-}
-.cookie:hover {
-  text-decoration: underline 1px;
 }
 </style>
