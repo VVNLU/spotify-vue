3356f9673b8b189822b53edfddf2cb36764d0c8f
Vivian Lu <vivianlu512@gmail.com>
2023-07-03 21:55:03 +0800

    add vuex and

--- spotify/src/main.js
+++ spotify/src/main.js
@@ -2,19 +2,21 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
 import "@mdi/font/css/materialdesignicons.css";
-import VueCookies from "vue3-cookies";
+// import VueCookies from "vue3-cookies";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import { store } from "./store";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
 app.use(VueAxios, axios);
-app.use(VueCookies, {
-  expireTimes: "30d",
-  //   path: "/",
-  domain: "",
-  secure: true,
-  sameSite: "None",
-});
+// app.use(VueCookies, {
+//   expireTimes: "30d",
+//   //   path: "/",
+//   domain: "",
+//   secure: true,
+//   sameSite: "None",
+// });
 app.mount("#app");
