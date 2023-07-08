692f99cd7c3f1e2646a7a971d46ae4bc51d418fa
Vivian Lu <vivianlu512@gmail.com>
2023-07-08 16:05:27 +0800

    add pinia

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from "./router/index";
 import "@mdi/font/css/materialdesignicons.css";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import { store } from "./store";
+import { store } from "./stores";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
 app.use(VueAxios, axios);
+app.use(createPinia());
 app.mount("#app");
