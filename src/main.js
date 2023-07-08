478984f294417ed80f12a527cd7ea68be82f70a8
Vivian Lu <vivianlu512@gmail.com>
2023-07-08 11:57:18 +0800

    delete cookie

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
 import "@mdi/font/css/materialdesignicons.css";
-// import VueCookies from "vue3-cookies";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import { store } from "./store";
@@ -12,11 +11,4 @@ const app = createApp(App);
 app.use(router);
 app.use(store);
 app.use(VueAxios, axios);
-// app.use(VueCookies, {
-//   expireTimes: "30d",
-//   //   path: "/",
-//   domain: "",
-//   secure: true,
-//   sameSite: "None",
-// });
 app.mount("#app");
