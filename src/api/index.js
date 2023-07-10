2758ffb12d0bd4bea2c01474722baf833c46f9f9
Vivian Lu <vivianlu512@gmail.com>
2023-07-10 18:40:58 +0800

    add after 60 min delete localStorage

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import router from "../router";
 
 const request = axios.create({
   baseURL: "https://api.spotify.com/v1/",
@@ -6,7 +7,6 @@ const request = axios.create({
     "Content-Type": "application/json",
   },
 });
-
 request.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
@@ -15,6 +15,32 @@ request.interceptors.request.use(
   },
   function (error) {
     return Promise.reject(error);
+  }
+);
+request.interceptors.response.use(
+  (response) => {
+    if (response.status === 200) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(response);
+    }
+  },
+  (error) => {
+    if (error.response.status) {
+      switch (error.response.status) {
+        case 401:
+          alert("超過API限制時間，請重新登入！");
+          localStorage.removeItem("username");
+          localStorage.removeItem("firebaseToken");
+          localStorage.removeItem("token");
+          setTimeout(() => {
+            router.replace({
+              path: "/login",
+            });
+          }, 5000);
+      }
+    }
+    return Promise.reject(error.response);
   }
 );
 
